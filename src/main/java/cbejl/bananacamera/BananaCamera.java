ddba3f0615cbebacbe4c6124a4e388773f226834
cbejl <superslavyanin@gmail.com>
2024-03-23 02:22:58 +0300

    FINALLY. WORK.

--- src/main/java/cbejl/bananacamera/BananaCamera.java
+++ src/main/java/cbejl/bananacamera/BananaCamera.java
@@ -1,17 +1,76 @@
 package cbejl.bananacamera;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public final class BananaCamera extends JavaPlugin {
 
+    private static BananaCamera instance;
+    private static File configf, locationsf, messagesf;
+    public static FileConfiguration config, locations, messages;
+    private static List<Map<?, ?>> locationsList;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        createFiles();
+        getCommand("bcamera").setExecutor(new CameraCommand());
+        getCommand("bcamera").setTabCompleter(new CameraCommand());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        instance = null;
+    }
+
+    public void createFiles() {
+        locationsf = new File(getDataFolder(), "locations.yml");
+        configf = new File(getDataFolder(), "config.yml");
+        messagesf = new File(getDataFolder(), "messages.yml");
+
+        if (!locationsf.exists()) {
+            locationsf.getParentFile().mkdirs();
+            saveResource("locations.yml", false);
+        }
+        if (!configf.exists()) {
+            configf.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+        if (!messagesf.exists()) {
+            messagesf.getParentFile().mkdirs();
+            saveResource("messages.yml", false);
+        }
+
+        config = new YamlConfiguration();
+        locations = new YamlConfiguration();
+        messages = new YamlConfiguration();
+
+        try {
+            config.load(configf);
+            locations.load(locationsf);
+            messages.load(messagesf);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        locationsList = locations.getMapList("locations");
+    }
+
+    public static FileConfiguration getMessages() {
+        return messages;
+    }
+
+    public static BananaCamera getInstance() {
+        return instance;
+    }
+
+    public static List<Map<?,?>> getLocationsList() {
+        return locationsList;
     }
 }
--- src/main/java/cbejl/bananacamera/CameraCommand.java
+++ src/main/java/cbejl/bananacamera/CameraCommand.java
@@ -1,29 +1,120 @@
 package cbejl.bananacamera;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class StartCameraCommand implements CommandExecutor {
-    private int cameraLocationIndex = 0;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CameraCommand implements CommandExecutor, TabCompleter {
+    private BukkitTask task;
+    private Player cameraPlayer;
+    private int locationIndex = 0;
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        BukkitTask task = new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (cameraLocationIndex < BananaCamera.getLocationsList().size() - 1) {
-                    cameraLocationIndex += 1;
-                } else {
-                    cameraLocationIndex = 0;
+        if (commandSender.hasPermission("bcamera.control") || !(commandSender instanceof Player)) {
+            if (strings.length > 0) {
+                if (strings[0].equals("run") && (task == null || task.isCancelled())) {
+                    task = new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            if (BananaCamera.getInstance().getServer().getPlayer(BananaCamera.config.getString("cameraName")) != null) {
+                                cameraPlayer = BananaCamera.getInstance().getServer()
+                                        .getPlayer(BananaCamera.config.getString("cameraName"));
+                            } else {
+                                BananaCamera.getInstance().getLogger()
+                                        .info(BananaCamera.getMessages().getString("camera.notfound"));
+                                commandSender.sendMessage(Component.text(BananaCamera.getMessages().getString("camera.notfound")));
+                                task.cancel();
+                                return;
+                            }
+
+                            try {
+                                assert cameraPlayer != null;
+
+                                ArrayList rotation = (ArrayList) BananaCamera.getLocationsList().get(locationIndex).get("rotation");
+                                ArrayList position = (ArrayList) BananaCamera.getLocationsList().get(locationIndex).get("xyz");
+                                String world = (String) BananaCamera.getLocationsList().get(locationIndex).get("world");
+
+                                cameraPlayer.setAllowFlight(true);
+                                cameraPlayer.setGameMode(GameMode.CREATIVE);
+                                cameraPlayer.setInvisible(true);
+                                cameraPlayer.setFlying(true);
+                                cameraPlayer.teleport(new Location(BananaCamera.getInstance().getServer().getWorld(world),
+                                        ((Number)position.get(0)).doubleValue(), ((Number)position.get(1)).doubleValue(), ((Number)position.get(2)).doubleValue(),
+                                        ((Number)rotation.get(0)).floatValue(), ((Number)rotation.get(1)).floatValue()));
+                                cameraPlayer.setFlying(true);
+                                cameraPlayer.showTitle(Title.title(
+                                        Component.text((String) BananaCamera.getLocationsList().get(locationIndex).get("title")),
+                                        Component.text((String) BananaCamera.getLocationsList().get(locationIndex).get("subtitle"))));
+                                if (locationIndex < BananaCamera.getLocationsList().size() - 1) {
+                                    locationIndex += 1;
+                                } else {
+                                    locationIndex = 0;
+                                }
+
+                            } catch (Exception e) {
+                                BananaCamera.getInstance().getLogger()
+                                        .info(BananaCamera.getMessages().getString("camera.notfound"));
+                                task.cancel();
+                                return;
+                            }
+                        }
+
+                    }.runTaskTimer(BananaCamera.getInstance(), 0,
+                            BananaCamera.config.getLong("period") * 20L);
+                    BananaCamera.getInstance().getLogger().info(BananaCamera.getMessages().getString("camera.task.run"));
+                    commandSender.sendMessage(Component.text(BananaCamera.getMessages().getString("camera.task.run")));
+                    return true;
+                } else if (strings[0].equals("run") && (task != null && !task.isCancelled())) {
+                    BananaCamera.getInstance().getLogger().info(BananaCamera.getMessages().getString("camera.task.already.run"));
+                    commandSender.sendMessage(Component.text(BananaCamera.getMessages().getString("camera.task.already.run")));
+                    return false;
+                } else if (strings[0].equals("stop")) {
+                    if (task == null || task.isCancelled()) {
+                        BananaCamera.getInstance().getLogger().info(BananaCamera.getMessages().getString("camera.task.already.stop"));
+                        commandSender.sendMessage(Component.text(BananaCamera.getMessages().getString("camera.task.already.stop")));
+                        return false;
+                    } else if (task != null) {
+                        task.cancel();
+                        cameraPlayer.setAllowFlight(true);
+                        cameraPlayer.setInvisible(false);
+                        cameraPlayer.kick(Component.text(BananaCamera.getMessages().getString("camera.task.stop")));
+                        BananaCamera.getInstance().getLogger().info(BananaCamera.getMessages().getString("camera.task.stop"));
+                        commandSender.sendMessage(Component.text(BananaCamera.getMessages().getString("camera.task.stop")));
+                        return true;
+                    }
+                } else if (strings[0].equals("reload")) {
+                    BananaCamera.getInstance().createFiles();
+                    BananaCamera.getInstance().getLogger().info(BananaCamera.getMessages().getString("camera.config.reload"));
+                    commandSender.sendMessage(Component.text(BananaCamera.getMessages().getString("camera.config.reload")));
+                    return true;
                 }
-                
             }
-        }.runTaskLater(BananaCamera.getInstance(),
-                (int) BananaCamera.getLocationsList().get(cameraLocationIndex).get("time") * 20);
+        }
 
         return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        ArrayList<String> list = new ArrayList<>();
+        if (strings.length == 1) {
+            list.add("run");
+            list.add("stop");
+            list.add("reload");
+        }
+        return list;
     }
 }
